27f4ef4524e461c5f4566f345765fea2a79b9ba8
Giuliano Picilli <giulianopicilli@hevolus.it>
2021-05-30 12:39:14 +0200

    test UTF-8

--- src/main/java/game/jsonParser/JsonParser.java
+++ src/main/java/game/jsonParser/JsonParser.java
@@ -3,17 +3,29 @@ package game.jsonParser;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class JsonParser
 {
     public static <T> T GetClassFromJson(String pathFileJson, Class<T> typeClass) throws IOException
     {
-        String content = new String (Files.readAllBytes(Paths.get(pathFileJson)));
-        Gson gson = new Gson();
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Stream stream = Files.lines( Paths.get(pathFileJson), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> stringBuilder.append(s).append("\n"));
+            Gson gson = new Gson();
+            return gson.fromJson(stringBuilder.toString(), typeClass);
 
-        return gson.fromJson(content, typeClass);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 
