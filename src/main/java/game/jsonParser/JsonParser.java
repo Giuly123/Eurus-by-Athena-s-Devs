b37a221027c9091266fbad7354a19b1358f2e5ca
Riccardo Ranieri <r.ranieri23@studenti.uniba.it>
2021-06-13 18:15:52 +0200

    eseguita documentazione javadoc 3

--- src/main/java/game/jsonParser/JsonParser.java
+++ src/main/java/game/jsonParser/JsonParser.java
@@ -3,16 +3,31 @@ package game.jsonParser;
 import com.google.gson.Gson;
 import game.gameUtilities.Utilities;
 
-
+/**
+ * Classe utilities per la serializzazione e
+ * deserializzazione dei file json.
+ */
 public class JsonParser
 {
+    /**
+     * Deserializza una classe da un file json.
+     * @param pathFileJson path del file json
+     * @param typeClass tipo della classe in cui deve essere fatto il parse del file
+     * @param <T> tipo generico
+     * @return l'istanza della classe popolata con le informazioni parsate dal file json
+     */
     public static <T> T getClassFromJson(String pathFileJson, Class<T> typeClass)
     {
         Gson gson = new Gson();
         return gson.fromJson(Utilities.readFile(pathFileJson), typeClass);
     }
 
-
+    /**
+     * Serializza una classe secondo lo standard json.
+     * @param object oggetto da serializzare
+     * @param <T> tipo generico
+     * @return stringa contenente la classe serializzata
+     */
     public static <T> String serializeClassToJson(T object)
     {
         Gson gson = new Gson();
